88b4aa4e83b3b6bc55482ed7111647ff6190abc8
Anil_Ramcharran <anil.gitme@gmail.com>
2020-12-08 20:03:27 -0500

    boilerplates setup done

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import * as yup from 'yup';
+
+export default function Form() {
+
+
+
+    return (
+        <form>
+            
+        </form>
+    )
+}
